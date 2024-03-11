ab5fcc03130ecd691b37c64a10199a6868498022
Henry1241 <enri09ley@outlook.com>
2024-03-10 17:29:46 -0700

    login
    Inicio del la pantalla del login

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,54 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import App from "@/App.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <router-link class="navbar-brand" to="/">
+      <img
+        src="@/assets/portaLogo.png"
+        alt=""
+        width="200"
+        height="200"
+        class="d-inline-block align-text-top"
+      />
+    </router-link>
+    <h1>Bienvendo</h1>
+
+    <div class="mb-3">
+        <label for="" class="form-label">Email</label>
+        <input
+          type="email"
+          class="form-control"
+          name=""
+          id=""
+          aria-describedby="emailHelpId"
+          placeholder="ejemplo123@portasi.com"
+        />
+      </div>
+      <br>
+      <div class="mb-3">
+        <label for="" class="form-label">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          name=""
+          id=""
+          placeholder="**********"
+        />
+      </div>
+      <button type="button" class="btn btn-outline-success">Aceptar</button>
+      <div class="d-grid gap-2">
+      </div>
+      
+  </div>
+      
 </template>
+
+<style>
+.container {
+  min-height: 100vh;
+  position: static;
+  align-items: center;
+}
+</style>
